5e0fefafd4de0613e309e3cfdbf942dd93f4ef85
Adam Mahameed <adamm@ownbackup.com>
2022-11-13 14:46:51 +0200

    delete unnecessary files

--- app/main.py
+++ app/main.py
@@ -1,29 +1,31 @@
 """Main FastAPI app instance declaration."""
 
 from fastapi import FastAPI
+from db.database import SessionLocal
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.trustedhost import TrustedHostMiddleware
 
 from app.api.api import api_router
-from app.core import config
 
-app = FastAPI(
-    title=config.settings.PROJECT_NAME,
-    version=config.settings.VERSION,
-    description=config.settings.DESCRIPTION,
-    openapi_url="/openapi.json",
-    docs_url="/",
-)
+app = FastAPI()
 app.include_router(api_router)
 
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 # Sets all CORS enabled origins
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[str(origin) for origin in config.settings.BACKEND_CORS_ORIGINS],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=[str(origin) for origin in config.settings.BACKEND_CORS_ORIGINS],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 # Guards against HTTP Host Header attacks
-app.add_middleware(TrustedHostMiddleware, allowed_hosts=config.settings.ALLOWED_HOSTS)
+# app.add_middleware(TrustedHostMiddleware, allowed_hosts=config.settings.ALLOWED_HOSTS)
